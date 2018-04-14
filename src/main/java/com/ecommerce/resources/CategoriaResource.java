1ff20dd3ad75f1f3cfe7869d8c0e2bdfb8b43e43
Valdir Santilli <valdirsantilli@gmail.com>
2018-04-14 09:24:24 -0300

    Criação da classe de dominio Categoria

--- src/main/java/com/ecommerce/resources/CategoriaResource.java
+++ src/main/java/com/ecommerce/resources/CategoriaResource.java
@@ -1,8 +1,13 @@
 package com.ecommerce.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ecommerce.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -10,7 +15,12 @@ public class CategoriaResource {
 
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar(){
-		return "Rest está funcionando!";
+	public List<Categoria> listar(){
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listCat = Arrays.asList(cat1, cat2);
+		
+		return listCat;
 	}
 }
