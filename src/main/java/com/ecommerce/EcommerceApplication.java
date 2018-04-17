581c01ce6f456fcf2b69370eb1f0fec83d6df05d
Valdir Santilli <valdirsantilli@gmail.com>
2018-04-16 23:45:36 -0300

    Criando operação de inserção de categorias no banco de dados

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -1,12 +1,27 @@
 package com.ecommerce;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EcommerceApplication {
+import com.ecommerce.domain.Categoria;
+import com.ecommerce.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class EcommerceApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		categoriaRepository.saveAll(Arrays.asList(new Categoria(null,"Informática"), new Categoria(null, "Escritório")));
 	}
 }
