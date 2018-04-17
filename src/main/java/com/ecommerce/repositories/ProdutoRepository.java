70f4afdc84a506a5617d28b45ef59a4a52d159d2
Valdir Santilli <valdirsantilli@gmail.com>
2018-04-17 00:08:56 -0300

    Criando associação de produtos com as categorias

--- src/main/java/com/ecommerce/repositories/ProdutoRepository.java
+++ src/main/java/com/ecommerce/repositories/ProdutoRepository.java
@@ -3,8 +3,8 @@ package com.ecommerce.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.ecommerce.domain.Categoria;
+import com.ecommerce.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 }
