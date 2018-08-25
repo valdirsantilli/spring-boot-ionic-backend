908a8efdf74b57cd58c97d88a8dedc440f25c904
Valdir Santilli <valdirsantilli@gmail.com>
2018-08-25 00:55:40 -0300

    Criação do Endpoint para atualizar Categoria e adequação alterar do nome do metodo buscar para find dos services

--- src/main/java/com/ecommerce/services/ClienteService.java
+++ src/main/java/com/ecommerce/services/ClienteService.java
@@ -15,7 +15,7 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repository.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id:" + id + ", Tipo: "+ Cliente.class.getName()));
--- src/main/java/com/ecommerce/services/PedidoService.java
+++ src/main/java/com/ecommerce/services/PedidoService.java
@@ -15,7 +15,7 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repository;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repository.findById(id); 
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id:" + id + ", Tipo: "+ Pedido.class.getName()));
