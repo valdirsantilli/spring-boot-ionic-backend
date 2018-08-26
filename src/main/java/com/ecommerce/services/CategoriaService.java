d1fa55837fea0d5286c5ef383df3710bc1a8712c
Valdir Santilli <valdirsantilli@gmail.com>
2018-08-26 20:50:06 -0300

    Criação do Endpoint para deletar uma Categoria

--- src/main/java/com/ecommerce/services/CategoriaService.java
+++ src/main/java/com/ecommerce/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ecommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.domain.Categoria;
 import com.ecommerce.repositories.CategoriaRepository;
+import com.ecommerce.services.exceptions.DataIntegrityException;
 import com.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repository.deleteById(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+		}
 	}
 }
