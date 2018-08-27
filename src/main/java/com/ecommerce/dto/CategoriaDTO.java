2d080f34a08c0e3a74059603ae5da911046505b9
Valdir Santilli <valdirsantilli@gmail.com>
2018-08-26 21:02:19 -0300

    Criação do Endpoint para listar todas categorias e criacao do CategoriaDTO

---
+++ src/main/java/com/ecommerce/dto/CategoriaDTO.java
@@ -0,0 +1,37 @@
+package com.ecommerce.dto;
+
+import java.io.Serializable;
+
+import com.ecommerce.domain.Categoria;
+
+public class CategoriaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Integer id;
+	public String nome;
+	
+	public CategoriaDTO() {
+	}
+
+	public CategoriaDTO(Categoria categoria) {
+		this.id = categoria.getId();
+		this.nome = categoria.getNome();
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
