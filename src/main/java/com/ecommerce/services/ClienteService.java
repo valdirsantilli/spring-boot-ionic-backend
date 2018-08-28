1f601f8ae48059111f0c128adcc33693aba38060
Valdir Santilli <valdirsantilli@gmail.com>
2018-08-27 23:01:28 -0300

    Ajustando a deleção de clientes para apagar os endereços relacionados

--- src/main/java/com/ecommerce/services/ClienteService.java
+++ src/main/java/com/ecommerce/services/ClienteService.java
@@ -55,7 +55,7 @@ public class ClienteService {
 		try {
 			repository.deleteById(id);
 		}catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+			throw new DataIntegrityException("Não é possível excluir porque há pedidos relacionadas");
 		}
 	}
 	
