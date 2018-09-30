21cba93cec66a7e766aa8ca269f58afa1fb2dc3e
Valdir Santilli <valdirsantilli@gmail.com>
2018-09-30 19:01:25 -0300

    Implementado o Esqueci a senha

--- src/main/java/com/ecommerce/services/EmailService.java
+++ src/main/java/com/ecommerce/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.ecommerce.domain.Cliente;
 import com.ecommerce.domain.Pedido;
 
 public interface EmailService {
@@ -15,4 +16,6 @@ public interface EmailService {
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
