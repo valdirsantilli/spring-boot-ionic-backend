d2c0d747689c2934412f3195aeb390e2a184b0a8
Valdir Santilli <valdirsantilli@gmail.com>
2018-09-30 09:35:27 -0300

    Implementando validacao do token e autorizacao

--- src/main/java/com/ecommerce/config/security/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/security/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ecommerce.security.JWTAuthenticationFilter;
+import com.ecommerce.security.JWTAuthorizationFilter;
 import com.ecommerce.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/com/ecommerce/security/JWTUtil.java
+++ src/main/java/com/ecommerce/security/JWTUtil.java
@@ -1,10 +1,13 @@
 package com.ecommerce.security;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
@@ -23,5 +26,37 @@ public class JWTUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + expiration))
 				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
 				.compact();
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			String username = claims.getSubject();
+			LocalDateTime expirationDate = claims.getExpiration()
+													.toInstant()
+													.atZone(ZoneId.systemDefault())
+													.toLocalDateTime();
+			LocalDateTime now = LocalDateTime.now();
+			if(username != null && expirationDate != null && now.isBefore(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		}catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if(claims != null) {
+			return claims.getSubject();
+		}
+		return null;
 	}
 }
