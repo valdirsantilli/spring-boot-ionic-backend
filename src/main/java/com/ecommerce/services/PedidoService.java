9f2f7a15329ccd59826321207e051a0f44bf5748
Valdir Santilli <valdirsantilli@gmail.com>
2018-09-30 10:29:13 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/ecommerce/services/PedidoService.java
+++ src/main/java/com/ecommerce/services/PedidoService.java
@@ -4,16 +4,24 @@ import java.time.LocalDateTime;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ecommerce.domain.Categoria;
+import com.ecommerce.domain.Cliente;
 import com.ecommerce.domain.ItemPedido;
 import com.ecommerce.domain.PagamentoComBoleto;
 import com.ecommerce.domain.Pedido;
 import com.ecommerce.domain.enums.EstadoPagamento;
+import com.ecommerce.domain.enums.Perfil;
 import com.ecommerce.repositories.ItemPedidoRepository;
 import com.ecommerce.repositories.PagamentoRepository;
 import com.ecommerce.repositories.PedidoRepository;
+import com.ecommerce.security.UserSS;
+import com.ecommerce.services.exceptions.AuthorizationException;
 import com.ecommerce.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -62,5 +70,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 }
